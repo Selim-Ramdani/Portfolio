ae958a64d6b480b4521a8cdab84184e21b152d3d
Selim-Ramdani <selimcodeur@gmail.com>
2022-06-27 18:23:42 +0200

    About page

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -9,6 +9,7 @@ const App = () => {
     <div className="app">
       <Navbar />
       <Header />
+      <About />
     </div>
   )
 }
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import './About.scss';
+import { images } from '../../constants';
+import "./About.scss";
 
 const About = () => {
+  const about = [
+    { title: "Frontend Development", description: "React/Next.js - Redux/Toolkit", imgUrl: images.about01 },
+    { title: "MERN Stack", description: "Je suis un bon développeur", imgUrl: images.about02 },
+    { title: "Backend Development", description: "Ruby on Rails", imgUrl: images.about03 },
+    { title: "Déploiements", description: "Heroku, Netlify, Vercel", imgUrl: images.about04 }
+  ];
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <h2 className="head-text">
+        Je sais qu'un <span>Bon développement</span><br /> signifie de<span> bonnes affaires</span>
+      </h2>
+
+      <div className="app__profiles">
+        {about.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{marginTop:20}}>{about.title}</h2>
+            <p className="p-text" style={{marginTop:10}}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default About;
