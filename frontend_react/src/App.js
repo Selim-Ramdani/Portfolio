c514db27de391f938cb598da7d15645643eea852
Selim-Ramdani <selimcodeur@gmail.com>
2022-06-27 14:42:02 +0200

    second commit - CRA using sanity as a backend

---
+++ frontend_react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components/Navbar';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
