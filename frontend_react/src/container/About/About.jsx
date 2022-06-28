a98da25fdc5923a63652f1f7e7883b55ef7b7e45
Selim-Ramdani <selimcodeur@gmail.com>
2022-06-28 09:53:17 +0200

    sanity client connected to react

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-import { images } from '../../constants';
 import "./About.scss";
+import { urlFor, client } from '../../client';
 
-const About = () => {
-  const about = [
-    { title: "Frontend Development", description: "React/Next.js - Redux/Toolkit", imgUrl: images.about01 },
-    { title: "MERN Stack", description: "Je suis un bon développeur", imgUrl: images.about02 },
-    { title: "Backend Development", description: "Ruby on Rails", imgUrl: images.about03 },
-    { title: "Déploiements", description: "Heroku, Netlify, Vercel", imgUrl: images.about04 }
-  ];
+
+  const About = () => {
+    const [abouts, setAbouts] = useState([]);
+
+    useEffect(() => {
+      const query = '*[_type == "abouts"]';
+
+      client.fetch(query)
+      .then((data) => {
+        setAbouts(data);
+      });
+    }, []);
+    
   return (
     <>
       <h2 className="head-text">
@@ -18,7 +24,7 @@ const About = () => {
       </h2>
 
       <div className="app__profiles">
-        {about.map((about, index) => (
+        {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
@@ -26,7 +32,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{marginTop:20}}>{about.title}</h2>
             <p className="p-text" style={{marginTop:10}}>{about.description}</p>
           </motion.div>
